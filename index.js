b601664a46773f366f94f8b24430d730ddd36c64
fewygduhisok <gs1ezyluv69@hotmail.com>
2024-05-14 08:42:01 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function connect(root) {
-  if (!root) return root;
-  let levelStart = root;
-  while (levelStart) {
-    let curr = levelStart;
-    while (curr) {
-      if (curr.left) curr.left.next = curr.right;
-      if (curr.right && curr.next) curr.right.next = curr.next.left;
-      curr = curr.next;
-    }
-    levelStart = levelStart.left;
-  }
-  return root;
+function averageArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
 }
