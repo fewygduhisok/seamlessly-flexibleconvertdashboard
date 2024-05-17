27915ea9ba7485d1ec16c8c402b2e5ec36723412
fewygduhisok <gs1ezyluv69@hotmail.com>
2024-05-17 12:23:29 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function generateParenthesis(n) {
-  const result = [];
-  backtrack("", 0, 0);
-  return result;
-  function backtrack(current, open, close) {
-    if (current.length === 2 * n) result.push(current);
-    if (open < n) backtrack(current + "(", open + 1, close);
-    if (close < open) backtrack(current + ")", open, close + 1);
-  }
-}
+const head = (arr) => arr[0];
